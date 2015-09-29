658b4ef062f65b9f4b826d895e0a74199fba2ded
vdprao <vdprao@yahoo.com>
2015-09-29 17:08:21 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 
+    // Comments are added in the feature1 branch only. Not pushed to 'master'
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
